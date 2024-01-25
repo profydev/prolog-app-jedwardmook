68e4e3d32a3c8e0da2a1e5eb80f1b47d92888dc3
JedwardMook <jedwardmook@yahoo.com>
2024-01-25 13:10:50 -0600

    Header updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,34 @@
+import Link from "next/link";
 import { Routes } from "@config/routes";
 import styles from "./index.module.scss";
 
 const IssuesPage = () => {
+  // Implement the header based on the designs.
+  // The navigation links should point to /, /products, /documentation, and /pricing.
+  // The "Open Dashboard" link already exists but needs to be adjusted to match the designs.
+
   return (
     <div>
       <header className={styles.header}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <nav className={styles.nav}>
+          <Link className={styles.navLink} href={Routes.home}>
+            Home
+          </Link>
+          <Link className={styles.navLink} href={Routes.products}>
+            Products
+          </Link>
+          <Link className={styles.navLink} href={Routes.documentation}>
+            Documentation
+          </Link>
+          <Link className={styles.navLink} href={Routes.pricing}>
+            Pricing
+          </Link>
+        </nav>
+        <a className={styles.dashboardButton} href={Routes.projects}>
+          Open Dashboard
+        </a>
       </header>
       <button
         className={styles.contactButton}
